1f858936ca780a7b3b87b3da7e153c53287215c1
deviwlndrr <wdevi443@gmail.com>
2024-06-07 17:26:03 +0700

    push delete

---
+++ js/controller/delete.js
@@ -0,0 +1,17 @@
+function deleteData(IDHAPUS) {
+    var presensiId = IDHAPUS;
+    var target_url = "https://ws-deviwlndr-59b3a0157dd7.herokuapp.com/delete/" + presensiId;
+
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+
+    fetch(target_url, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            alert(result.message);
+            location.reload();
+        })
+        .catch(error => console.log('Error:', error));
+}
